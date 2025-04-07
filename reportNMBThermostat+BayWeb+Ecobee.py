9910366aa2a39d8aa9cc032ad65d31101b8ff868
Jim Mollmann <jim.mollmann@gmail.com>
2025-04-07 08:11:16 -0400

    test minT for "None" rather than "False"

--- reportNMBThermostat+BayWeb+Ecobee.py
+++ reportNMBThermostat+BayWeb+Ecobee.py
@@ -177,7 +177,7 @@ def fmtTempsLine(tag, row):
     noData = '  None'
     noData = '     .'
     line = '{:>10s}'.format(tag) + ': (none)'
-    if row['minT']:
+    if row['minT'] is not None:
         period =  '{:>10s}'.format(tag)
         minT   = ' {:>5.1f}'.format(row['minT'])       if row['minT'] is not None else noData
         maxT   = ' {:>5.1f}'.format(row['maxT'])       if row['maxT'] is not None else noData
